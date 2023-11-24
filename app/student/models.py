16f66bf765b2a57943323fed4699c3cca176509d
Shabarish1403 <67517952+Shabarish1403@users.noreply.github.com>
2023-11-24 18:20:29 +0530

    Student models.py

---
+++ app/student/models.py
@@ -0,0 +1,30 @@
+from flask_security import UserMixin, RoleMixin
+
+class Role(db.Model, RoleMixin):
+    __tablename__ = 'role'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String, nullable=False, unique=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    active = db.Column(db.Boolean)
+    fs_uniquifier = db.Column(db.String, nullable=False, unique=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'), nullable=False)
+    
+class Student(db.Model):
+    __tablename__ = 'student'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    phone = db.Column(db.Integer(10), nullable=False)
+    rollno = db.Column(db.String, nullable=False, unique=True)
+    cgpa = db.Column(db.Float, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    completed_courses = db.relationship('Score', backref='student',cascade='all,delete-orphan')
+
+class Admin(db.Model):
+    __tablename__ = 'admin'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
