8954347914ac7addb399ac0aeee03c6bf6a6b09c
Shabarish1403 <67517952+Shabarish1403@users.noreply.github.com>
2023-11-28 22:15:20 +0530

    added database.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_security import Security, SQLAlchemyUserDatastore
 from flask_restful import Resource, Api
+from .config import LocalDevelopmentConfig, StageConfig
+from .database import db
+from flask_security import Security, SQLAlchemySessionUserDatastore, SQLAlchemyUserDatastore
 from app.student.models import User, Role
-from config import LocalDevelopmentConfig, StageConfig
+
 
 app = None
 api = None
@@ -11,6 +13,7 @@ api = None
 def create_app():
     app = Flask(__name__, template_folder="templates")
     print(os.getenv('ENV', "development"))
+
     if os.getenv('ENV', "development") == "production":
       app.logger.info("Currently no production config is setup.")
       raise Exception("Currently no production config is setup.")
@@ -25,31 +28,21 @@ def create_app():
       app.config.from_object(LocalDevelopmentConfig)
       print("pushed config")
     app.app_context().push()
+
     print("DB Init")
     db.init_app(app)
     print("DB Init complete")
     app.app_context().push()
     app.logger.info("App setup complete")
+
     # Setup Flask-Security
     user_datastore = SQLAlchemySessionUserDatastore(db.session, User, Role)
     security = Security(app, user_datastore)
+
     api = Api(app)
     app.app_context().push()   
-    
-    # # Create celery   
-    # celery = workers.celery
-
-    # # Update with configuration
-    # celery.conf.update(
-    #     broker_url = app.config["CELERY_BROKER_URL"],
-    #     result_backend = app.config["CELERY_RESULT_BACKEND"]
-    # )
-
-    # celery.Task = workers.ContextTask
-    # app.app_context().push()
-    # cache = Cache(app)
-    # app.app_context().push()
     print("Create app complete")
+    
     return app, api
 
 app, api = create_app()
