785cccaec0ba2160022f0f4fc1978c8c24cd1517
Sai Charan <saicharankmrs@gmail.com>
2023-12-04 07:37:39 +0530

    basic course controllers gets implemented

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, marshal_with, reqparse, fields
 from config import LocalDevelopmentConfig, StageConfig
 from database import db
 from flask_security import Security, SQLAlchemySessionUserDatastore, SQLAlchemyUserDatastore
 from app.student.models import User, Role
-
+import json
 
 app = None
 api = None
@@ -48,6 +48,59 @@ def create_app():
     return app, api
 
 app, api = create_app()
+
+from app.course.models import *
+course_fields = {
+    "id": fields.String,
+    "name": fields.String,
+    "acronym": fields.String,
+    "credits": fields.Integer,
+    "prerequisites": fields.String,
+    "corequisites": fields.String,
+    "description": fields.String
+}
+
+course_parser = reqparse.RequestParser()
+course_parser.add_argument("id")
+course_parser.add_argument("name")
+course_parser.add_argument("description")
+
+class CourseApi(Resource):
+    def get(self, id=None):
+        if id is None:
+          courses_required = Course.query.all()
+          try:
+            l = []
+            for course_required in courses_required:
+              course_json = {}
+              course_json["id"] = course_required.id
+              course_json["name"] = course_required.name
+              course_json["description"] = course_required.description
+              l.append(course_json)
+            return l, 200
+          except AttributeError:
+            return "Invalid Course ID"
+        else:
+          course_required = Course.query.filter_by(id=id).first()
+          try:
+            course_json = {}
+            course_json["id"] = course_required.id
+            course_json["name"] = course_required.name
+            course_json["description"] = course_required.description
+            return course_json, 200
+          except AttributeError:
+            return "Invalid Course ID"
+          
+    @marshal_with(course_fields)
+    def post(self):
+            
+    
+       
+       
+
+api.add_resource(CourseApi, "/api/courses/", "/api/courses/<string:id>/")
+
+
 
 if __name__ == '__main__':
   # Run the Flask app
