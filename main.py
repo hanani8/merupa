b07b1a9324fdc84c56ca91d159ecda8b32cf2d28
Sai Charan <saicharankmrs@gmail.com>
2023-12-04 22:59:13 +0530

    POST implemented for CourseApi

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, make_response
 from flask_restful import Resource, Api, marshal_with, reqparse, fields
 from config import LocalDevelopmentConfig, StageConfig
 from database import db
 from flask_security import Security, SQLAlchemySessionUserDatastore, SQLAlchemyUserDatastore
 from app.student.models import User, Role
 import json
+from werkzeug.exceptions import HTTPException
 
 app = None
 api = None
@@ -53,17 +54,24 @@ from app.course.models import *
 course_fields = {
     "id": fields.String,
     "name": fields.String,
-    "acronym": fields.String,
-    "credits": fields.Integer,
-    "prerequisites": fields.String,
-    "corequisites": fields.String,
-    "description": fields.String
+}
+
+course_ratings_fields = {
+  "id": fields.Integer,
+  "course_id": fields.String,
+  "student_id": fields.String
 }
 
 course_parser = reqparse.RequestParser()
 course_parser.add_argument("id")
 course_parser.add_argument("name")
 course_parser.add_argument("description")
+course_parser.add_argument("rating_type")
+course_parser.add_argument("rating_value")
+course_parser.add_argument("student_id")
+
+
+
 
 class CourseApi(Resource):
     def get(self, id=None):
@@ -91,16 +99,43 @@ class CourseApi(Resource):
           except AttributeError:
             return "Invalid Course ID"
           
-    @marshal_with(course_fields)
+    @marshal_with(course_ratings_fields)
     def post(self, id):
       args = course_parser.parse_args()
-      rating_type = args.get("rating_type")
-      rating_value = args.get("rating_value")
-      print(rating_type, rating_value)
+      rating_type = args["rating_type"]
+      rating_value = args["rating_value"]
+      student_id = args["student_id"]
+      fetched_rating = Rating.query.filter_by(type = rating_type).first()
+      fetched_student = Student.query.filter_by(id = student_id).first()
+      course = Course.query.filter_by(id = id).first()
+      if fetched_rating is not None:
+        if course is not None:
+          if fetched_student is not None:
+            course_rating_record = CourseRating(
+                course_id = id,
+                student_id = student_id,
+                rating_id = fetched_rating.id,
+                value = rating_value
+            )
+            db.session.add(course_rating_record)
+            db.session.commit()
+            return course_rating_record, 201 
+          else:
+           raise BusinessValidationError(status_code=400,error_code="S001",error_message="Student Not Found")
+        else:
+           raise BusinessValidationError(status_code=400,error_code="C001",error_message="Course Not Found")
+      else:
+         raise BusinessValidationError(status_code=400,error_code="R001",error_message="Rating Type Not Found")
+      
+         
       
     
        
-       
+class BusinessValidationError(HTTPException):
+    def __init__(self,status_code, error_code, error_message):
+        message = {"Error Code": error_code, "Message": error_message}
+        self.response = make_response(json.dumps(message),status_code)  
+
 
 api.add_resource(CourseApi, "/api/courses/", "/api/courses/<string:id>/", "/api/courses/<string:id>/rating")
 
