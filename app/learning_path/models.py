a3a7ed2ae6d6afdd756856c9ee32ff9c319f8560
Himadri Dixit <130587221+himadridixit@users.noreply.github.com>
2023-12-06 14:51:58 +0530

    Update models.py

--- app/learning_path/models.py
+++ app/learning_path/models.py
@@ -1,6 +1,15 @@
+from app.database import db
+from app.student.models import *
 
 class LearningPath(db.Model):
     __tablename__ = "learningpath"
     id = db.Column(db.Integer, autoincrement = True, primary_key = True)
     path = db.Column(db.String, nullable = False)
-    rating = db.Column(db.Integer)
+    upvote = db.Column(db.Integer, default = 0)
+
+class LPUpvote(db.Model):
+    __tablename__ = "lpupvote"
+    id = db.Column(db.Integer, autoincrement = True, primary_key = True)
+    user_id =  db.Column(db.Integer, db.ForeignKey("user.id"))
+    learning_path_id = db.Column(db.Integer, db.ForeignKey("learningpath.id"))
+
