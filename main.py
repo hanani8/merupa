d771b148b24fd79fb66f55929b05749a49faf585
Himadri Dixit <130587221+himadridixit@users.noreply.github.com>
2023-12-06 14:56:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -61,6 +61,9 @@ from app.student.routes import StudentAPI
 api.add_resource(CourseApi, "/api/courses/", "/api/courses/<string:id>/", "/api/courses/<string:id>/rating")
 api.add_resource(StudentAPI, "/api/student/<int:student_id>", "/api/admin/students")
 >>>>>>> fb7a7e6eac14cc4bad21b82cc8d7895e55b335ef
+from app.learning_path.routes import LearningPathAPI, LearningPathsAPI
+api.add_resource(LearningPathsAPI, "/api/learningpaths")
+api.add_resource(LearningPathAPI, "/api/learningpath/<int:id>", "/api/learningpath/upvote")
 
 if __name__ == '__main__':
   # Run the Flask app
