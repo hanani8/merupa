4a78fe4250e4817f5e38482151ea2536fed16292
hanani8 <hananidasilva@gmail.com>
2023-12-10 12:14:18 +0530

    Course Tests

--- tests/course.py
+++ tests/course.py
@@ -4,11 +4,11 @@ base_url = "http://localhost:5000"
 
 from app import init_app
 app = init_app()
-from app.student.models import CourseRating
+from app.course.models import CourseRating
 from app.database import db
 
 def test_course_correct_id():
-    endpoint = base_url + "/api/course/course/MA1001"
+    endpoint = base_url + "/api/course/MA1001"
     response = requests.get(endpoint)
     data = response.json()
     assert response.status_code == 200
@@ -52,28 +52,29 @@ def test_course_successful_rating():
     data = response.json()
     assert response.status_code == 201
     assert data["course_id"] == "MA1001"
-    # with app.app_context():
-    #     CourseRating.query.filter_by(student_id = 3, course_id = "MA1001").delete()
-    #     db.session.commit()
+    with app.app_context():
+        CourseRating.query.filter_by(student_id = 3, course_id = "MA1001").delete()
+        db.session.commit()
 
 def test_course_unsuccessful_edit_feedback():
-    endpoint = base_url + "api/course/CS1001/rating"
-    response = requests.put(endpoint, json={"rating_type":"Course Content", "rating_value":7.5, "student_id": 33})
+    endpoint = base_url + "/api/course/CS1001/rating"
+    response = requests.put(endpoint, json={"rating_type":"Course Content", "rating_value":7.5, "student_id": 3})
     data = response.json()
+    print(data)
     assert response.status_code == 400
     assert data["error_code"] == "CR001"
     assert data["error_message"] == "Course Rating for the given Student-Course Combination Not Found"
 
 def test_course_unsuccessful_edit_feedback_wrong_rating_type():
-    endpoint = base_url + "api/course/CS1001/rating"
-    response = requests.put(endpoint, json={"rating_type":"Custom Rating", "rating_value":7.5, "student_id": 33})
+    endpoint = base_url + "/api/course/CS1001/rating"
+    response = requests.put(endpoint, json={"rating_type":"Custom Rating", "rating_value":7.5, "student_id": 3})
     data = response.json()
     assert response.status_code == 400
     assert data["error_code"] == "R001"
-    assert data["error_message"] == "Rating Type Not Found"
+    assert data["error_message"] == "Rating Not Found"
 
 def test_course_unsuccessful_edit_feedback_wrong_course_id():
-    endpoint = base_url + "api/course/ABC1001/rating"
+    endpoint = base_url + "/api/course/ABC1001/rating"
     response = requests.put(endpoint, json={"rating_type":"Course Content", "rating_value":7.5, "student_id": 33})
     data = response.json()
     assert response.status_code == 400
@@ -81,7 +82,7 @@ def test_course_unsuccessful_edit_feedback_wrong_course_id():
     assert data["error_message"] == "Course Not Found"
 
 def test_course_unsuccessful_edit_feedback_wrong_student_id():
-    endpoint = base_url + "api/course/CS1001/rating"
+    endpoint = base_url + "/api/course/CS1001/rating"
     response = requests.put(endpoint, json={"rating_type":"Course Content", "rating_value":7.5, "student_id": 3300})
     data = response.json()
     assert response.status_code == 400
@@ -89,11 +90,12 @@ def test_course_unsuccessful_edit_feedback_wrong_student_id():
     assert data["error_message"] == "Student Not Found"
 
 def test_course_successful_edit_feedback():
-    endpoint = base_url + "api/course/MA1001/rating"
-    response = requests.put(endpoint, json={"rating_type":"Course Support", "rating_value":8.5, "student_id": 3})
+    endpoint = base_url + "/api/course/MA1003/rating"
+    response = requests.put(endpoint, json={"rating_type":"Course Support", "rating_value":8.5, "student_id": 2})
     data = response.json()
+    print(data)
     assert response.status_code == 200
-    assert data["course_id"] == "MA1001"
+    assert data["course_id"] == "MA1003"
 
 def test_course_no_previous_feedback():
     endpoint = base_url + "/api/course/CS2004/feedback"
@@ -115,14 +117,14 @@ def test_course_successful_downvote():
     response = requests.put(endpoint, json={"vote": "downvote", "student_id": 3})
     data = response.json()
     assert response.status_code == 200
-    assert data['course_id'] == "CS4004"
+    assert data['course_id'] == "CS2004"
 
 def test_course_successful_upvote():
     endpoint = base_url + "/api/course/CS2004/feedback/vote"
     response = requests.put(endpoint, json={"vote": "upvote", "student_id": 3})
     data = response.json()
     assert response.status_code == 200
-    assert data['course_id'] == "CS4004"
+    assert data['course_id'] == "CS2004"
 
 def test_course_delete_feedback():
     endpoint = base_url + "/api/course/CS2004/feedback"
