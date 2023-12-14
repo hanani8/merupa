6af558a568a24ed2b0ab74f92fa2c8878e28220a
hanani8 <hananidasilva@gmail.com>
2023-12-14 23:52:22 +0530

    Custom Unauthorized Response

--- app/security.py
+++ app/security.py
@@ -4,3 +4,7 @@ from app.database import db
 
 user_datastore = SQLAlchemySessionUserDatastore(db.session, User, Role)
 security = Security()
+
+@security.unauthorized_handler
+def unauthorized_handler():
+    return {"error": False, "msg": "NOT_LOGGED_IN"}, 200
